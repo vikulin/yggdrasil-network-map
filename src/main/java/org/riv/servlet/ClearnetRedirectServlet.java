c47f6230594044c4334f9b2463aee04c73bd6f23
Vadym Vikulin <vadym.vikulin@gmail.com>
2021-12-11 00:44:36 +0200

    1. added clearnet test page

---
+++ src/main/java/org/riv/servlet/ClearnetRedirectServlet.java
@@ -0,0 +1,23 @@
+package org.riv.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ClearnetRedirectServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String remoteIp = req.getRemoteAddr();
+		if(remoteIp.startsWith("fc") || remoteIp.startsWith("fd")) {
+			System.out.println("RiV-mesh. IP:"+remoteIp);
+			resp.sendRedirect(req.getContextPath() + "/");
+			return;
+		}
+        System.out.println("Redirection to clearnet page. IP:"+remoteIp);
+        resp.sendRedirect(req.getContextPath() + "/clearnet");
+    }
+
+}
