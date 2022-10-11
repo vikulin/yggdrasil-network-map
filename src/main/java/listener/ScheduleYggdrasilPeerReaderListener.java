f3ec17d46b0fe9628ad017cbadb5758c4d703ca5
Vadym Vikulin <vadym.vikulin@rivchain.org>
2022-10-11 13:41:20 +0300

    1. added sctp schema support

--- src/main/java/listener/ScheduleYggdrasilPeerReaderListener.java
+++ src/main/java/listener/ScheduleYggdrasilPeerReaderListener.java
@@ -29,7 +29,7 @@ public class ScheduleYggdrasilPeerReaderListener implements ServletContextListen
 	public static final Map<String, LinkedHashMap<String, PeerInfo>> peersPerCountry = new TreeMap<String, LinkedHashMap<String, PeerInfo>>();
 
 	Pattern IPv4_PEER_REGEXP = Pattern.compile(
-			"(((tcp)|(udp)|(tls))\\://(((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)\\.){3}(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d))\\:\\d{1,5})");
+			"(((tcp)|(sctp)|(tls))\\://(((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)\\.){3}(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d))\\:\\d{1,5})");
 	Pattern IPv6_PEER_REGEXP = Pattern.compile(
 			"((tcp)|(sctp)|(tls))\\://\\[(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))\\]\\:\\d{1,5}");
 	Pattern DOMAIN_NAME_PEER_REGEXP = Pattern.compile("(((tcp)|(udp)|(tls))\\://([\\w\\-]+\\.)+[a-zA-Z]+\\:\\d{1,5})");
